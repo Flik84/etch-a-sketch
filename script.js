8874d7f3986fa5b565697699cccaa3bf9f43cbc4
Flik84 <chris.polvi@hotmail.ca>
2022-12-09 14:35:35 -0800

    Unstyled but working version. Includes button prompting user for size of grid

---
+++ script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
+const randomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const changeColor = function () {
+    color = `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+    return color;
+}
+
+function removeGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+button.addEventListener('click', function () {
+    removeGrid(container);
+    const gridSize = Number(prompt('Please enter number of squares per side (Max 100): '));
+    if (gridSize >= 1 && gridSize <= 100) {
+        for (i = 0; i < Math.pow(gridSize, 2); i++) {
+            const div = document.createElement('div')
+            div.classList.add('square')
+            container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+            container.appendChild(div);
+            div.addEventListener('mouseover', function () {
+                div.style.backgroundColor = changeColor(); 
+            });
+        }
+    } else alert('Please choose a number between 1 and 100.')
+});
+
+
+
+
+
+
