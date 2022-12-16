e58db2477990bad1346cb04554307a1af8562940
Flik84 <chris.polvi@hotmail.ca>
2022-12-16 15:35:08 -0800

    Replace prompt for size input with slider

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const container = document.querySelector('.container');
-const button = document.querySelector('button');
+const gridButton = document.querySelector('.grid-button');
+const colorButton = document.querySelector('.color-button')
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.slider-number')
+
+
+slider.oninput = function() {
+  sliderValue.innerHTML = this.value;
+}
+
+
 
 const randomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
@@ -16,26 +26,44 @@ function removeGrid(parent) {
     }
 }
 
-
-button.addEventListener('click', function () {
+gridButton.addEventListener('click', function () {
     removeGrid(container);
-    const gridSize = Number(prompt('Please enter number of squares per side (Max 100): '));
+    const gridSize = slider.value;
     if (gridSize >= 1 && gridSize <= 100) {
         for (i = 0; i < Math.pow(gridSize, 2); i++) {
             const div = document.createElement('div')
-            div.classList.add('square')
+            div.classList.add('square');
+            if (colorButton.classList.contains('active')) {
+                div.classList.add('black');
+            } else div.classList.add('white')
             const squareSizeH = (screen.width / 3.7) / (gridSize);
             const squareSizeW = 2.4 * ((screen.width / 3.7) / (gridSize));
             div.style.width = `${squareSizeW}px`;
             div.style.height = `${squareSizeH}px`;
             container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
             container.appendChild(div);
+            container.style.boxShadow = '0px 0px 10px 4px';
+            const containerWidth = gridSize * squareSizeW;
+            container.style.width = `${containerWidth}px`;
             div.addEventListener('mouseover', function () {
-                div.style.backgroundColor = changeColor(); 
+            div.style.backgroundColor = changeColor(); 
             });
         }
     } else alert('Please choose a number between 1 and 100.')
 });
+
+colorButton.addEventListener('click', function() {
+    colorButton.classList.toggle('active');
+    const squares = document.querySelectorAll('.square');
+    console.log(squares);
+    if (colorButton.classList.contains('active')) {
+        squares.forEach(function (square) { 
+            square.style.backgroundColor = 'black';   
+        })
+    } else {
+        squares.forEach(function (square) { 
+        square.style.backgroundColor = 'white';
+})}});
 
 
 
